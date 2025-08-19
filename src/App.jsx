074acdd530e4923c2bf017c3bffe859c68cde97d
Dev-Abdullah-s <creatortechniques6@gmail.com>
2025-08-19 12:41:43 +0500

    Fix GitHub Pages routing with basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/mainLayout";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
@@ -13,33 +8,25 @@ import CategoryPage from "./pages/CategoryPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Cardetail from "./pages/Cardetail";
 import OTP from "./pages/otp";
+import Userprofile from "./pages/userprofile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useState } from "react";
-
-import Userprofile from "./pages/userprofile";
 
 export default function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/category/:id" element={<CategoryPage />} />
-          <Route path="/Car_details" element={<Cardetail />} />
-          <Route path="/otp-varification" element={<OTP />} />
-          <Route path="/UserDashboard" element={<Userprofile />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </>
-    )
-  );
-
   return (
     <>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="category/:id" element={<CategoryPage />} />
+          <Route path="Car_details" element={<Cardetail />} />
+          <Route path="otp-varification" element={<OTP />} />
+          <Route path="UserDashboard" element={<Userprofile />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
